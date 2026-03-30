d5502ea227fb30a43b94034ec39e4616c832c858
hymanhai <1184987178@qq.com>
2026-03-30 16:10:32 +0800

    Day 4: 抵达 信丰，余额 1000 元 - 游记: 2026-03-30-信丰.md - 图片: 2026-03-30-信丰.png - 状态更新

---
+++ test_awk.sh
@@ -0,0 +1,9 @@
+awk -v date="2026-03-31" -v city="上海" -v price="100元" -v wallet="900元" -v file="2026-03-31-上海.md" '
+  /^\| -/ {
+    print
+    print "| " date " | " city " | " price " | " wallet " | [查看](./" file ") |"
+    next
+  }
+  /\| <br \/>/ { next }
+  { print }
+' /Users/hymanhai/RoamShrimp/data/journals/index.md
